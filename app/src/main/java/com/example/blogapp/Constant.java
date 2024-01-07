e5a0c0324bc4d97fc00d5bc76c970cdd5aa25be1
xuanlocgs2000 <xuanlocboya@gmail.com>
2024-01-07 09:08:59 +0700

    authencation

---
+++ app/src/main/java/com/example/blogapp/Constant.java
@@ -0,0 +1,11 @@
+package com.example.blogapp;
+
+public class Constant {
+    public static final String URL ="http://192.168.1.3/";
+    public static final String HOME =URL+"api";
+    public static final String LOGIN =HOME+"/login/";
+    public static final String USER =HOME+"/user/";
+    public static final String REGISTER =HOME+"/register/";
+    public static final String SAVE_USER_INFO =HOME+"/userInfo/";
+//    public static final String POST =HOME+"api";
+}
